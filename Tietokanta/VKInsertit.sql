b0865cd967b309647003f0dd7cfc7c95970b7dbe
Eemeli Antikainen <eemeli.antikainen@edu.savonia.fi>
2022-01-10 14:19:08 +0200

    Lisäsin migraatioprojekteja ja päivitin tietokantaa

---
+++ Tietokanta/VKInsertit.sql
@@ -0,0 +1,29 @@
+insert into tax (taxPercentage) values (24);
+insert into tax (taxPercentage) values (14);
+insert into tax (taxPercentage) values (10);
+insert into tax (taxPercentage) values (0);
+
+insert into size (size) values("XS");
+insert into size (size) values("S");
+insert into size (size) values("M");
+insert into size (size) values("L");
+insert into size (size) values("XL");
+insert into size (size) values("XXL");
+insert into size (size) values("XXXL");
+
+insert into color (color) values("White");
+insert into color (color) values("Black");
+insert into color (color) values("Green");
+insert into color (color) values("Blue");
+insert into color (color) values("Red");
+insert into color (color) values("Yellow");
+insert into color (color) values("Fuchsia");
+insert into color (color) values("Silver");
+insert into color (color) values("Gray");
+insert into color (color) values("Olive");
+insert into color (color) values("Purple");
+insert into color (color) values("Maroon");
+insert into color (color) values("Aqua");
+insert into color (color) values("Lime");
+insert into color (color) values("Teal");
+insert into color (color) values("Navy");
